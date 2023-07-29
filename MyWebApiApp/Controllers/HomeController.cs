dfc35e248cae8652f4286431a5715647bf09b457
Dion.DucNN <ducnn@dion.vn>
2023-07-29 15:29:35 +0700

    Feature/DE-05 Tạo Api submitForm:

---
+++ MyWebApiApp/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyWebApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpPost("add")]
+        public IActionResult SubmitForm([FromBody] UserDTO obj )
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            return Ok(obj);
+
+        }
+
+    }
+    public class UserDTO
+    {
+        [Required]
+        [MaxLength(10,ErrorMessage = "Tên không được quá 10 kí tự")]
+        public string Name { get; set; }
+        [Required]
+        [Range(18,60, ErrorMessage = "Tuổi phải lớn hơn 18 và nhỏ hơn 60 tuổi")]
+        public int Age { get; set; }
+        public int Id { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ MyWebApiApp/Controllers/TestPathVariablesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestPathVariablesController : Controller
+    {
+        [HttpGet]
+        [Route("test-path-variables/{id}/{action}")]
+        public IActionResult TestPathVariables(int id, string action)
+        {
+            // Xử lý các path variable tại đây
+            // Ví dụ: Trả về một object JSON chứa các giá trị path variable
+            var result = new
+            {
+                Id = id,
+                Action = action
+            };
+
+            // Trả về JSON data
+            return Ok(result);
+        }
+    }
+}
